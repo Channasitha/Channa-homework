eae7970419c74fe845d279c6b66b2794e4a1aed4
Channasitha <59702672+Channasitha@users.noreply.github.com>
2020-02-17 22:19:52 +0700

    ngfor
    exercise loop for

--- src/app/menulist/menulistchild.component.ts
+++ src/app/menulist/menulistchild.component.ts
@@ -6,12 +6,18 @@ import { stringify } from 'querystring';
 @Component({
   selector: 'app-menulistchild',
   templateUrl: './menulistchild.component.html',
+  styles: [`
+    .odd { display: none; }
+    `],
 })
 
 export class MenulistchildComponent  {
 
   @Input()  chlmsg: string;
   @Output() sendback = new EventEmitter<string>();
+
+  items = ['One', 'Two', 'Three','Four','Five','Six','Seven','eight','Nine','Ten'];
+
   action = false;
   msg:string;
   
